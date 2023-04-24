3e55a5303c8f27cc97bc8222928e0689082c5c1f
annlen <annsofi.len@gmail.com>
2023-04-24 19:55:27 +0300

    add packages

--- src/main.js
+++ src/main.js
@@ -4,19 +4,15 @@ import App from './App.vue'
 //import './assets/main.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import router from './router'
+import store from "./store";
 
-createApp(App).use(router).mount('#app')
 
-// testing the pr triggery
-console.log('testing');
-// va
-// va 2
-// noja 3
-// 4
-// 5
-// 6
-// 7
-// ´8 från dev
+const app = createApp(App)
+.use(router)
+.use(store)
+.mount('#app')
 
-// tsting again
+
+
