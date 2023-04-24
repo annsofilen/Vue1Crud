19cdeae32681055a63da3d837a330306ea5b2f22
annlen <annsofi.len@gmail.com>
2023-04-24 19:34:13 +0300

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,23 +2,24 @@
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+    <nav class="navbar navbar-expand-sm bg-light navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Notes</a>
+            <a class="navbar-brand text-muted" href="#">Ann-Sofis Store <i class="bi bi-peace"></i></a>
             <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul class="navbar-nav mr-auto mt-2 ">
                     <!-- use router-link component for navigation. -->
                     <!-- specify the link by passing the `to` prop. -->
                     <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
                     <li class="nav-item active">
-                        <router-link class="nav-link" to="/">Home</router-link>
+                        <router-link class="nav-link text-muted" to="/">Home</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/About">About</router-link>
+                        <router-link class="nav-link text-muted" to="/About">About</router-link>
                     </li>
                 </ul>
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
